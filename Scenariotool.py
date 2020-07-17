dfb59582a85c28e33c0bd2668317dde8d7b43b8c
Zhang Siyuan <1611986066@qq.com>
2020-07-17 21:56:02 +0800

    base scenario ui V.20

--- Scenariotool.py
+++ Scenariotool.py
@@ -6,26 +6,23 @@ from Basescenario import Ui_BaseScenario
 
 
 class MainWindow(QtGui.QMainWindow):
-    def __int__(self):
-        super(MainWindow, self).__init__()  # 父类方法的初始
-        self.user_clicked()
+    def __init__(self):
+        super(MainWindow, self).__init__()  # 父类方法的初始化
+        self.ui_main = Ui_MainWindow()
+        self.ui_main.setupUi(self)
+        self.win_base = QtGui.QMainWindow()
+        self.ui_base = Ui_BaseScenario()
+        self.user_click()
 
-    def user_clicked(self):
-        self.connect(self.BaseButton, QtCore.SIGNAL('clicked()'), self.base_scenario())
+    def user_click(self):
+        self.connect(self.ui_main.BaseButton, QtCore.SIGNAL('clicked()'), self.base_scenario)
 
-    @staticmethod
-    def base_scenario():
-        # app_base = QtGui.QApplication(sys.argv)
-        win_base = QtGui.QMainWindow()
-        ui_base = Ui_BaseScenario()
-        ui_base.setupUi(win_base)
-        win_base.show()
-        # sys.exit(app_base.exec_())
+    def base_scenario(self):
+        self.ui_base.setupUi(self.win_base)
+        self.win_base.show()
 
 
 app = QtGui.QApplication(sys.argv)
 main = MainWindow()
-ui = Ui_MainWindow()  # 利用创建的界面类创建一个对象
-ui.setupUi(main)  # 将创建的界面嵌入MainWindow（也即QtGui.QMainWindow）类创建的对象）
 main.show()
 sys.exit(app.exec_())
